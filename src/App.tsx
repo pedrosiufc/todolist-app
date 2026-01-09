97d6e320e6b146f64bad4364326fab64a0b80481
pedrosiufc <pedrosiufc@gmail.com>
2026-01-09 13:24:14 -0300

    feat: inserida as imagens texto da logo e botão para alternar modo do tema.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import IconSun from "/images/icon-sun.svg";
+
 function App() {
-  return <p className="text-bright-blue">Hello World</p>   
-  
+  return (
+    <main className="bg-neutral-very-dark-blue h-screen">
+      <div className="bg-[url('/images/bg-desktop-dark.jpg')] h-80 bg-cover bg-center">
+        <header>
+          <h1>TODO</h1>
+          <button>
+            <img src={IconSun} alt="" />
+          </button>
+        </header>
+      </div>
+    </main>
+  );
 }
 
 export default App;
