06f064f0fcf0c01e2abff75d64edb0cca915203e
pedrosiufc <pedrosiufc@gmail.com>
2026-01-11 11:40:26 -0300

    feat: criada  a pasta compenents para dividir as atribuições.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import IconSun from "/images/icon-sun.svg";
+import TodoHeader from "./componets/TodoHeader";
 
 function App() {
   return (
     <main className="bg-neutral-very-dark-blue h-screen">
       <div className="bg-[url('/images/bg-desktop-dark.jpg')] h-80 bg-cover bg-center">
-        <header className="flex justify-between max-w-[43.75rem] m-auto mb-6 pt-20">
-          <h1 className="text-white text-[2.5rem] font-bold tracking-[1rem]">TODO</h1>
-          <button>
-            <img src={IconSun} alt="Alternar Tema" />
-          </button>
-        </header>
+      <TodoHeader></TodoHeader>       
       </div>
     </main>
   );
